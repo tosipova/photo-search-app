56ef4d633be5ba6990b751e6fb8b0d9db40d2ed4
Tatiana Osipova <advancerealgroup@gmail.com>
2020-07-25 13:14:10 +0200

    first changes, resetting pictures

--- index.js
+++ index.js
@@ -22,27 +22,19 @@ const eventCallback = event => {
     fetch(`https://api.unsplash.com/search/photos/?client_id=aoY1Nj_WIbaZG1De7H_D_Q_HOJvrsAqnBHufRc4Dv68&query=${query}`)
         .then(response => response.json())
         .then(json => {
+            const description = json.results[0].description
+            const thumbUrl = json.results[0].urls.thumb
+            const image = document.createElement("img")
 
-          const  description = json.results[0].description
-          const  thumbUrl = json.results[0].urls.thumb
 
-          console.log(description, thumbUrl)
+            image.src = thumbUrl
+            contentEl.appendChild(image)
 
-          // contentEl
-
-          // create el for description
-          // add description
-          // render description
-
-          
-          // create el for image
-          // add image
-          // render image
-
-          const image = document.createElement("img")
-          image.src = thumbUrl
-          
-          contentEl.appendChild(image)
+        }).then(() => {
+            const numberOfImages = contentEl.childElementCount
+            if (numberOfImages > 5) {
+                contentEl.innerHTML = ""
+            }
         })
 };
 searchFormEl.addEventListener("submit", eventCallback);
