f72bbb67e6b5b8f72a8c01b0a22fab518a866a25
Tatiana Osipova <advancerealgroup@gmail.com>
2020-07-28 09:38:46 +0200

    Render title

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
 const searchFormEl = document.querySelector("#search-form")
 const queryEl = document.querySelector("#query")
-const contentEl = document.querySelector("#result")
-
+const mainContent = document.querySelector("#result")
 
 
 const eventCallback = event => {
     event.preventDefault();
-
     const query = queryEl.value;
-
-
     fetch(`https://api.unsplash.com/search/photos/?client_id=aoY1Nj_WIbaZG1De7H_D_Q_HOJvrsAqnBHufRc4Dv68&query=${query}&orientation=squarish`)
         .then(response => response.json())
         .then(json => {
-
-            contentEl.innerHTML = ""
-
-            // Взять 5 карточек из json.results
-            // Для каждой карточки создать разметку
-            // Для каждой карточки разместить данные из json.results
-            // Каждую карточку должны отобразить
-            // Отобразить 5 карточек
-
             const cards = json.results.slice(0, 5);
 
-            // for (let i = 0; i < cards.length; i++) {
-            //     const thumbUrl = json.results[i].urls.thumb
-            //     const image = document.createElement("img")
-            //     image.src = thumbUrl
-            //     contentEl.appendChild(image)
-            // }
-
+            mainContent.innerHTML = ""
+            queryEl.value = ""
+        
             cards.forEach(card => {
-                const thumbUrl = card.urls.thumb
-                // TODO: adddescription
-                // const description = card.description
+                const parent = document.createElement("div")
+                const title = document.createElement("div")
                 const image = document.createElement("img")
                 
-                image.src = thumbUrl
+                title.innerHTML = card.alt_description
+                image.src = card.urls.thumb
                 image.classList.add('img-thumbnail')
-                
 
-                contentEl.appendChild(image)
+                parent.appendChild(title)
+                parent.appendChild(image)
+                mainContent.appendChild(parent)
+
             });
 
-            queryEl.value = ""
         })
 };
 searchFormEl.addEventListener("submit", eventCallback);
