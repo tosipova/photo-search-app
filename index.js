00d63ef851c4d2a007b6626ad86c7233d3d2bd55
Tatiana Osipova <advancerealgroup@gmail.com>
2020-08-04 10:22:53 +0200

    add pagination

--- index.js
+++ index.js
@@ -1,27 +1,88 @@
 const searchFormEl = document.querySelector("#search-form")
 const queryEl = document.querySelector("#query")
 const mainContent = document.querySelector("#results")
+const paginationEl = document.querySelector('#pagination');
 
 
 const API_KEY = 'aoY1Nj_WIbaZG1De7H_D_Q_HOJvrsAqnBHufRc4Dv68';
 const API_URL = 'https://api.unsplash.com/search/photos/'
 const ORIENTATION = 'squarish';
-let perPage = 5;
+const PER_PAGE = 1;
 
 
+let page = 1;
+let query = '';
+searchFormEl.defaultValue = "dog"
 
-const eventCallback = event => {
+
+const submitFormCallback = event => {
     event.preventDefault();
-    const query = queryEl.value;
-    const preFixUrl = `${API_URL}?client_id=${API_KEY}&query=${query}&orientation=${ORIENTATION}`
-    const fullApiUrl = `${preFixUrl}&per_page=${perPage}`;
-    searchFormEl.defaultValue = "dog"
+    query = queryEl.value;
+    page = 1;
+
+    fetchPhotos()
+}
+
+const onPaginationClickCallback = event => {
+    const paginationEls = document.querySelectorAll('.page-item');
+    // ['<li class="page-item active"><a class="page-link" href="#">1</a></li>',
+    //  '<li class="page-item active"><a class="page-link" href="#">2</a></li>',
+    //  '<li class="page-item active"><a class="page-link" href="#">3</a></li>']
+    [...paginationEls].forEach(el => {
+        el.classList.remove('active')
+    })
+
+    // event.target === <a class="page-link" href="#">2</a>
+    page = Number(event.target.innerText);
+    event.target.parentNode.classList.add('active');
+    // <li class="page-item active"><a class="page-link" href="#">2</a></li>
+
+
+
+    fetchPhotos()
+}
+
+searchFormEl.addEventListener("submit", submitFormCallback);
+
+paginationEl.addEventListener("click", onPaginationClickCallback);
+
+
+function fetchPhotos() {
+    const preFixUrl = `${API_URL}?client_id=${API_KEY}&query=${query}&orientation=${ORIENTATION}`;
+    const fullApiUrl = `${preFixUrl}&per_page=${PER_PAGE}&page=${page}`;
+
 
     fetch(fullApiUrl)
         .then(response => response.json())
-        .then(json => {
-            const cards = json.results;
-            console.log(cards)
+        .then(({ results: cards, total_pages: totalPages }) => {
+
+            // json.results;
+            // json.total_pages
+
+            // =>
+            // const { results, total_pages } = json;
+            // const cards = results;
+            // const totalPages = total_pages;
+            // cards,totalPages, results, total_pages
+
+            // => 
+            // const { results: cards, total_pages: totalPages } = json;
+            // cards, totalPages
+
+            // totalPages
+            // <li class="page-item">
+            //     <a class="page-link" href="#">3</a>
+            // </li>
+
+            
+            paginationEl.insertAdjacentHTML('beforeend', `
+                <li class="page-item">
+                    <a class="page-link" href="#">${totalPages}</a>
+                </li>
+            `)
+
+
+            mainContent.innerHTML = ""
             queryEl.value = ""
 
             cards.forEach(card => {
@@ -44,16 +105,16 @@ const eventCallback = event => {
                 parent.appendChild(cardBody)
                 parent.appendChild(image)
 
-                // Добавить только, если description
-                cardBody.appendChild(title)
+                // Boolean(card.description)
+                // Boolean(null) => false
+                // Boolean('Cat') => true
+                if (card.description) {
+                    cardBody.appendChild(title)
+                }
 
                 cardBody.appendChild(description)
                 mainContent.appendChild(parent)
             });
 
-        })
-};
-
-searchFormEl.addEventListener("submit", eventCallback);
-
-
+        });
+}
